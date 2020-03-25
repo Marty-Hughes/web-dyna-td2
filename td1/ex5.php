66de3a6d10a3a47727365a3b61febbad295b7f14
marty hughes <21908919@etu.unicaen.fr>
2020-03-25 11:57:48 +0100

    ex5

---
+++ td1/ex5.php
@@ -0,0 +1,18 @@
+<?php
+$title='Exercice n°4 (Itérations)';
+include "include/header.php";
+include "include/fonctions.php";
+
+?>
+<a href="?l=2&c=2">Tableau 2 x 2</a><br>
+<a href="?l=5&c=5">Tableau 5 x 5</a><br>
+<a href="?l=10&c=8">Tableau 10 x 8</a><br>
+<hr>
+<?php
+$nbLignes=$_GET["l"]??10;
+$nbColonnes=$_GET["c"]??10;
+
+echo div("Test de message avec la fonction div");
+
+include "include/footer.php";
+ ?>
