64ef1b8c468f0c3fbd55b9eec133e346a3880365
marty hughes <21908919@etu.unicaen.fr>
2020-05-13 08:34:20 +0200

    1er commit du TP4

---
+++ tp4/app/controllers/SampleController.php
@@ -0,0 +1,19 @@
+<?php
+namespace controllers;
+ /**
+ * Controller SampleController
+ **/
+class SampleController extends ControllerBase{
+
+	public function index(){
+		$this->loadView("SampleController/index.html");
+	}
+}
+{
+
+/**
+ *
+ * @get("_default","name"=>"Home")
+ */
+public function index() {}
+}
